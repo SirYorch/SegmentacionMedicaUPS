a1976e878e4c38409c93775ee5e6e08d7ed43ead
xavier <xavi2000pi@gmail.com>
2025-11-25 16:25:33 -0500

    Cambios

--- main.cpp
+++ main.cpp
@@ -24,6 +24,10 @@ int ksizeTrack = 3;       // representará kernel = 2*ksizeTrack + 1
 int sigmaTrack = 10;      // representará sigma = sigmaTrack / 10.0
 int umbralMin = 0;
 int umbralMax = 255;
+int cx = 100;
+int cy = 100;
+int radioX = 50;
+int radioY = 50;
 
 int kernelSize = 3;
 
@@ -183,6 +187,23 @@ Rect btnEq(10, 60, 120, 40);
 
 // Estado de botones (si quieres alternar)
 
+
+Mat maskCircle(Mat &img, int cx, int cy, int rx, int ry) {
+    Mat mask = Mat::zeros(img.rows, img.cols, CV_8UC1);
+
+    // 1. Dibujar círculo blanco = zona visible
+    ellipse(mask, cv::Point(cx, cy), cv::Size(rx, ry), 0, 0, 360, Scalar(255), FILLED);
+
+    // 2. Crear salida blanca (color blanco completo)
+    Mat salida(img.rows, img.cols, img.type(), Scalar(255));
+
+    // 3. Copiar dentro del círculo la imagen original
+    img.copyTo(salida, mask);
+
+    return salida;
+}
+
+
 void onMouse(int event, int x, int y, int flags, void*) {
     if (event == EVENT_LBUTTONDOWN) {
         if (btnCLAHE.contains(cv::Point(x, y))) {
@@ -195,6 +216,22 @@ void onMouse(int event, int x, int y, int flags, void*) {
         }
         
     }
+
+    // Redibujar controles para feedback visual (opcional pero recomendado)
+        Mat panel(60, 300, CV_8UC3, Scalar(50, 50, 50));
+        
+        // // Color del botón según estado
+        // Scalar colorClahe = clahe ? Scalar(100, 255, 100) : Scalar(200, 200, 200);
+        // Scalar colorEq = eq ? Scalar(100, 255, 100) : Scalar(200, 200, 200);
+
+        // rectangle(panel, btnCLAHE, colorClahe, FILLED);
+        // putText(panel, "CLAHE", cv::Point(25, 35), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0));
+
+        // rectangle(panel, btnEq, colorEq, FILLED);
+        // putText(panel, "EQ HIST", cv::Point(35, 85), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0));
+
+        imshow("Controles", panel);
+
 }
 Mat open(Mat imagen, int kernelSize){
     // Retorna proceso de apertura, erosion y dilatacion
@@ -219,6 +256,216 @@ Mat close(Mat imagen, int kernelSize){
     return salida;
 }
 
+Mat defineBones(Mat imagen) {
+
+    int minBone = 150; 
+    int maxBone = 255;
+    int kernelBone = 1; // Kernel pequeño para no fusionar costillas cercanas
+
+    // 2. Umbralización (Lo más importante)
+    imagen = Umbrilize(imagen, minBone, maxBone);
+    imagen = close(imagen, kernelBone);
+    imagen = open(imagen, kernelBone);
+
+    return imagen;
+}
+
+Mat defineOrgan(Mat imagen, bool isLung) {
+
+    int cxdf, cydf, radioXdf, radioYdf;
+    int mindf, maxdf, kerneldf;
+    int sliceLimit;
+
+    double sigmadf = sigmaTrack / 10.0;
+    
+    if (isLung) {
+        // Parámetros para PULMONES (Umbral bajo, Apertura, Máscara grande)
+        cxdf = 253;
+        cydf = 264;
+        radioXdf = 210;
+        radioYdf = 147;
+        mindf = 0;
+        maxdf = 58;
+        kerneldf = 9;
+        sliceLimit = 127; 
+        
+    } else {
+
+        cxdf = 284;
+        cydf = 220;
+        radioXdf = 87;
+        radioYdf = 65;
+        mindf = 110;  // Umbral ajustado para tejido blando
+        maxdf = 170;
+        kerneldf = 3; 
+        sliceLimit = 40; 
+    }
+
+
+    if (currentSlice < sliceLimit) {
+        imagen = maskCircle(imagen, cxdf, cydf, radioXdf, radioYdf);
+        GaussianBlur(imagen, imagen, cv::Size(kerneldf, kerneldf), sigmadf);
+
+        // Umbralización
+        imagen = Umbrilize(imagen, mindf, maxdf);
+        
+        // Operación Morfológica Condicional
+        if (isLung) {
+            imagen = open(imagen, kernelSize); // Pulmones usan Apertura
+
+        } else {
+            imagen = open(imagen, kernelSize); // Corazón usa Cierre
+            imagen = close(imagen, kernelSize); // Corazón usa Cierre
+
+            vector<vector<cv::Point>> contours;
+            vector<Vec4i> hierarchy;
+            // Encontrar contornos externos
+            findContours(imagen, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+            double maxArea = 0;
+            int maxAreaIdx = -1;
+            // Buscar el índice del contorno con el área más grande
+            for (size_t i = 0; i < contours.size(); i++) {
+                double area = contourArea(contours[i]);
+                if (area > maxArea) {
+                    maxArea = area;
+                    maxAreaIdx = i;
+                }
+            }
+            
+            // Crear una máscara negra limpia
+            Mat cleanMask = Mat::zeros(imagen.size(), CV_8UC1);
+            
+            if (maxAreaIdx >= 0) {
+                drawContours(cleanMask, contours, maxAreaIdx, Scalar(255), FILLED);
+            }
+            
+            imagen = cleanMask;
+            
+        }
+
+    } else {
+        imagen = Mat::zeros(imagen.rows, imagen.cols, CV_8UC1);
+    }
+
+    return imagen;
+}
+
+
+Mat mergeMasks(Mat &imgOriginal, Mat &maskLung, float alpha = 0.3) {
+    Mat imgColor;
+
+    // Convertir a BGR si la original es gris
+    if (imgOriginal.channels() == 1)
+        cvtColor(imgOriginal, imgColor, COLOR_GRAY2BGR);
+    else
+        imgColor = imgOriginal.clone();
+
+    // Crear capa verde del mismo tamaño
+    Mat greenLayer(imgOriginal.rows, imgOriginal.cols, CV_8UC3, Scalar(0, 255, 0));
+
+    // Crear salida inicial como copia
+    Mat output = imgColor.clone();
+
+    // Aplicar overlay SOLO donde la máscara es 255
+    for (int i = 0; i < imgOriginal.rows; i++) {
+        for (int j = 0; j < imgOriginal.cols; j++) {
+
+            if (maskLung.at<uchar>(i, j) == 255) {
+
+                // output = (1 - alpha) * original + alpha * green
+                output.at<Vec3b>(i, j)[0] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[0] +
+                    alpha * greenLayer.at<Vec3b>(i, j)[0];
+
+                output.at<Vec3b>(i, j)[1] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[1] +
+                    alpha * greenLayer.at<Vec3b>(i, j)[1];
+
+                output.at<Vec3b>(i, j)[2] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[2] +
+                    alpha * greenLayer.at<Vec3b>(i, j)[2];
+            }
+        }
+    }
+
+    return output;
+}
+
+
+Mat mergeMasksHeart(Mat &imgOriginal, Mat &maskLung, float alpha = 0.3) {
+    Mat imgColor;
+
+    // Convertir a BGR si la original es gris
+    if (imgOriginal.channels() == 1)
+        cvtColor(imgOriginal, imgColor, COLOR_GRAY2BGR);
+    else
+        imgColor = imgOriginal.clone();
+
+    // Crear capa verde del mismo tamaño
+    Mat greenLayer(imgOriginal.rows, imgOriginal.cols, CV_8UC3, Scalar(0, 0, 255));
+
+    // Crear salida inicial como copia
+    Mat output = imgColor.clone();
+
+    // Aplicar overlay SOLO donde la máscara es 255
+    for (int i = 0; i < imgOriginal.rows; i++) {
+        for (int j = 0; j < imgOriginal.cols; j++) {
+
+            if (maskLung.at<uchar>(i, j) == 255) {
+
+                // output = (1 - alpha) * original + alpha * green
+                output.at<Vec3b>(i, j)[0] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[0] +
+                    alpha * greenLayer.at<Vec3b>(i, j)[0];
+
+                output.at<Vec3b>(i, j)[1] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[1] +
+                    alpha * greenLayer.at<Vec3b>(i, j)[1];
+
+                output.at<Vec3b>(i, j)[2] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[2] +
+                    alpha * greenLayer.at<Vec3b>(i, j)[2];
+            }
+        }
+    }
+
+    return output;
+}
+
+Mat mergeMasksBones(Mat &imgOriginal, Mat &maskBone, float alpha = 0.3) {
+    Mat imgColor;
+
+    if (imgOriginal.channels() == 1)
+        cvtColor(imgOriginal, imgColor, COLOR_GRAY2BGR);
+    else
+        imgColor = imgOriginal.clone();
+
+    // Color AMARILLO: (Blue=0, Green=255, Red=255)
+    Mat yellowLayer(imgOriginal.rows, imgOriginal.cols, CV_8UC3, Scalar(0, 255, 255));
+
+    Mat output = imgColor.clone();
+
+    for (int i = 0; i < imgOriginal.rows; i++) {
+        for (int j = 0; j < imgOriginal.cols; j++) {
+            if (maskBone.at<uchar>(i, j) == 255) {
+                output.at<Vec3b>(i, j)[0] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[0] +
+                    alpha * yellowLayer.at<Vec3b>(i, j)[0];
+
+                output.at<Vec3b>(i, j)[1] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[1] +
+                    alpha * yellowLayer.at<Vec3b>(i, j)[1];
+
+                output.at<Vec3b>(i, j)[2] =
+                    (1.0 - alpha) * imgColor.at<Vec3b>(i, j)[2] +
+                    alpha * yellowLayer.at<Vec3b>(i, j)[2];
+            }
+        }
+    }
+    return output;
+}
+
 
 
 
@@ -241,107 +488,86 @@ int main() {
     }
     namedWindow("Controles", WINDOW_AUTOSIZE);
 
-    namedWindow("Visor", WINDOW_AUTOSIZE);
-    namedWindow("VisorFiltro", WINDOW_AUTOSIZE);
-    namedWindow("Blured", WINDOW_AUTOSIZE);
-    namedWindow("Apertura", WINDOW_AUTOSIZE);
-    namedWindow("Close", WINDOW_AUTOSIZE);
-    namedWindow("ap+cl", WINDOW_AUTOSIZE);
-    namedWindow("cl+ap", WINDOW_AUTOSIZE);
-    
-
-    moveWindow("Visor", 300, 0);        // Mover la ventana "Visor"
-    moveWindow("Controles", 0, 0);
-    moveWindow("VisorFiltro",810, 0);
-    moveWindow("Blured", 1320, 0);
-    moveWindow("Apertura", 0, 800);
-    moveWindow("Close", 510, 800);
-    moveWindow("ap+cl", 1020, 800);
-    moveWindow("cl+ap", 1530, 800);
 
     createTrackbar("Slice",    "Controles", &currentSlice, imgs.size() - 1);
-    createTrackbar("KSize",    "Controles", &ksizeTrack, 50);  // límite arbitrario
-    createTrackbar("Sigma x10","Controles", &sigmaTrack, 100); // sigma max = 10.0
-
-    createTrackbar("KernelSize","Controles", &kernelSize, 100); // sigma max = 10.0
-
-    createTrackbar("Min", "Controles", &umbralMin, 255);  // límite arbitrari
-    createTrackbar("Max", "Controles", &umbralMax, 255); // sigma max = 10.0
+    //createTrackbar("KSize",    "Controles", &ksizeTrack, 50);  // límite arbitrario
     
-    Mat panel(160, 300, CV_8UC3, Scalar(50, 50, 50));
-
-
+    Mat panel(60, 300, CV_8UC3, Scalar(50, 50, 50));
 
     ///// BOTONES  NOS SIRVEN PARA DARLE ACCIONES A LA APLICACIÓN DESDE OTRA VENTANA
 
     setMouseCallback("Controles", onMouse);
 
-    // Botón CLAHE
-    rectangle(panel, btnCLAHE, Scalar(200, 200, 200), FILLED);
-    putText(panel, "CLAHE", cv::Point(25, 35), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0));
 
-    // Botón BLUR
-    rectangle(panel, btnEq, Scalar(200, 200, 200), FILLED);
-    putText(panel, "BLUR", cv::Point(35, 85), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0));
+    // Con pruebas llegamos a la idea de que el BLUR aplicado solo sirve para los pulmones
+    // Los huesos se ven mucho mejor con la ecualización y luego BLUR, 
+    // Y el corazon se ve mejor con 
+
+    // Los pulmones se ven mucho mejor solo con apertura
+
+    /// para aislar los pulmonesel circulo usamos. slices 0-127
+    // circulo en 253,264
+    // RadioX de 210
+    // radioY de 247
+
+    // El corazon se ven mucho mejor solo con cierre
+
+    /// para aislar los pulmonesel circulo usamos. slices 0-42
+    // circulo en 284,215
+    // RadioX de 89
+    // radioY de 66
+
 
     imshow("Controles", panel);
-    
-    
+    moveWindow("Controles", 0, 0);
 
 
-   while (true) {
-        Mat imagen = imgs[currentSlice].clone();
-        Mat imagenOriginal = imgs[currentSlice].clone();
-
-        int kernel = 2 * ksizeTrack + 1;
-        double sigma = sigmaTrack / 10.0;
-        
-        if(eq){
-            imagen = toEq(imagen);
-        }
-
-        if(clahe){
-            imagen = toClahe(imagen);
-        }
-        
-        GaussianBlur(imagen, imagen, cv::Size(kernel, kernel), sigma);
-        
-        Mat imagenBlur = imagen.clone();        
-
-        // Umbralice
-        // cout << umbralMin << " " << umbralMax;
-        Mat salidaUmbral = Umbrilize(imagen, umbralMin , umbralMax);
-
-        Mat Apertura = open(salidaUmbral, kernelSize);
-
-        Mat Close = close(salidaUmbral, kernelSize);
-
-        Mat apcl = close(Apertura, kernelSize);
-        Mat clap = open(Close, kernelSize);
+    namedWindow("Pulmones", WINDOW_AUTOSIZE);
+    namedWindow("CompletaPulmones", WINDOW_AUTOSIZE);
+    namedWindow("CorazonMascara", WINDOW_AUTOSIZE);
+    namedWindow("CompletaCorazon", WINDOW_AUTOSIZE); 
+    namedWindow("HuesosMascara", WINDOW_AUTOSIZE);
+    namedWindow("CompletaHuesos", WINDOW_AUTOSIZE); 
 
 
-        // Mostrar
-        imshow("Visor", salidaUmbral);
-        imshow("VisorFiltro", imagen);
-        imshow("Blured", imagenBlur);
-        imshow("Apertura", Apertura);
-        imshow("ap+cl", apcl);
-        imshow("cl+ap", clap);
-        imshow("Close", Close);
+    moveWindow("Pulmones", 300, 500);
+    moveWindow("CompletaPulmones", 300, 0);
+    moveWindow("CorazonMascara", 810, 500); 
+    moveWindow("CompletaCorazon", 810, 0); 
+    moveWindow("HuesosMascara", 1320, 500); 
+    moveWindow("CompletaHuesos", 1320, 0); 
+
+
+    while (true) {
+        Mat Original = imgs[currentSlice].clone();
+
+        // --- Procesamiento de Pulmones (como ya lo tenías) ---
+        Mat pulmonesMascara = imgs[currentSlice].clone();
+        pulmonesMascara = defineOrgan(pulmonesMascara, true); // Función que usa Apertura
+        Mat completaPulmones = mergeMasks(Original, pulmonesMascara, 0.3); // Overlay verde
+
+        imshow("Pulmones", pulmonesMascara);
+        imshow("CompletaPulmones", completaPulmones);
+
+
+        Mat corazonMascara = imgs[currentSlice].clone();
+        corazonMascara = defineOrgan(corazonMascara, false);
+        Mat completaCorazon = mergeMasksHeart(Original, corazonMascara, 0.3); // Overlay rojo
+
+        imshow("CorazonMascara", corazonMascara); // Muestra solo la máscara del corazón
+        imshow("CompletaCorazon", completaCorazon); // Muestra la imagen original con el corazón resaltado
+
+        Mat huesosMascara = imgs[currentSlice].clone();
+        huesosMascara = defineBones(huesosMascara);
+        Mat completaHuesos = mergeMasksBones(Original, huesosMascara, 0.4); // Alpha un poco más alto para que brillen
+
+        imshow("HuesosMascara", huesosMascara);
+        imshow("CompletaHuesos", completaHuesos);
 
         int k = waitKey(30);
         if (k == 27) break; 
     }
 
-
-
-    // Con pruebas llegamos a la idea de que el BLUR aplicado solo sirve para los pulmones
-    // Los huesos se ven mucho mejor con la ecualización y luego BLUR, 
-
-
-
-    // Los pulmones se ven mucho mejor solo con apertura
-
-
+    destroyAllWindows(); // Cierra todas las ventanas al salir
     return 0;
 }
